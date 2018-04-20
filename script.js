e091f2c6facdca532cfa17bcbaccaa715dddb89e
sarmsaad <sarah.msaad@yahoo.com>
2018-04-20 18:47:00 -0400

    ajax

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var button = document.getElementById('submitButton').innerHTML;
-if(button){
-  button.addEventListener('click', function(){
-    var memory1 = document.getElementById('mem1').innerHTML;
-    var memory2 = document.getElementById('mem2').innerHTML;
-    var memory3 = document.getElementById('mem3').innerHTML;
-    var result = [memory1, memory2, memory3];
-    console.log(result);
-    console.log("PUMPKIN");
-  });
+// var button = document.getElementById('submitButton').innerHTML;
+// if(button){
+//   button.addEventListener('click', function(){
+//     var memory1 = document.getElementById('mem1').innerHTML;
+//     var memory2 = document.getElementById('mem2').innerHTML;
+//     var memory3 = document.getElementById('mem3').innerHTML;
+//     var result = [memory1, memory2, memory3];
+//     console.log(result);
+//     console.log("PUMPKIN");
+//   });
 
-}
+// }
 
 
 var populate_img = function(img1, img2, img3){
