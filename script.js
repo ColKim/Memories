d40955f68e588bbd1847ca7c60346b0494585ff3
sarmsaad <sarah.msaad@yahoo.com>
2018-04-20 17:38:25 -0400

    connect

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 
+var 
+
 var populate_img = function(img1, img2, img3){
+    x = document.createElement("IMG");
+    x.setAttribute("src", img1);
     document.getElementById("img_one").appendChild(img1);
+    x = document.createElement("IMG");
+    x.setAttribute("src", img2);
     document.getElementById("img_two").appendChild(img2);
+    x = document.createElement("IMG");
+    x.setAttribute("src", img3);
     document.getElementById("img_three").appendChild(img3);
 }
